c5325426bd11f57ee2eadbd5f91a20bb99baee52
Dejan Vujacic <vujacicdejan98@gmail.com>
2023-03-24 13:23:27 +0100

    Fixed mobile version

--- script.js
+++ script.js
@@ -20,11 +20,6 @@ $(".start").click(function () {
     $(".start").text("Restart");
     nextSequence();
     started = true;
-    if (started) {
-      $(".start").click(function () {
-        startOver();
-      });
-    }
   }
 });
 
@@ -54,6 +49,7 @@ function checkAnswer(currentLevel) {
       $("body").removeClass("game-over");
     }, 200);
     $("#level-title").text("Game Over, Press Any Key to Restart");
+
     startOver();
   }
 }
